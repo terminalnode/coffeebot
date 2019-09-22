53749d56c7e0da9852ff62248c76ed43c4b3bdc4
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-22 22:18:55 +0200

    added some comments explaining what's going on

--- src/main/java/xyz/kazuthecat/coffeebot/commands/Ping.java
+++ src/main/java/xyz/kazuthecat/coffeebot/commands/Ping.java
@@ -2,14 +2,23 @@ package xyz.kazuthecat.coffeebot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+/* This function is a sort of demo function where all the various parts of the command
+ * class and important syntax stuff (mostly lambda functions) are explained for future reference. */
 
+// extends Command is, of course, what makes us able to instantiate this as a command
 public class Ping extends Command {
     public Ping() {
+        /* this.name/help/guildOnly are values used for various functions and for the
+           most part self explanatory. guildOnly makes it not work in DMs, help is
+           printed in the help menu, name is the name used in help menus and when
+           invoking the command (we can also set aliases) */
         this.name = "ping";
         this.help = "pings the bot";
         this.guildOnly = false;
     }
 
+    /* The execute function is part of the Command class and entails the events
+     * that occur when the command is triggered. */
     @Override
     protected void execute(CommandEvent event) {
         long time = System.currentTimeMillis();
@@ -22,12 +31,5 @@ public class Ping extends Command {
                         response -> response.editMessageFormat(
                                 "Pong! %d ms", (System.currentTimeMillis() - time)).queue()
                 );
-
-        event.getChannel().sendMessage("hehe").queue();
-
-        /*
-        * message = await channel.send("text")
-        * await message.edit("hehe")
-        * */
     }
 }

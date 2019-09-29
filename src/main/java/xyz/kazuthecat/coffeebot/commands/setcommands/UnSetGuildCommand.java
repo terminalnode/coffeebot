6f7535133729f3eb9d790656a5ab22e54c0f7501
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-29 05:12:41 +0200

    ?guildunset command implemented, admins can now unset guild settings

--- src/main/java/xyz/kazuthecat/coffeebot/commands/setcommands/UnSetGuildCommand.java
+++ src/main/java/xyz/kazuthecat/coffeebot/commands/setcommands/UnSetGuildCommand.java
@@ -3,6 +3,9 @@ package xyz.kazuthecat.coffeebot.commands.setcommands;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Guild;
+import xyz.kazuthecat.coffeebot.settings.SettingEnum;
 import xyz.kazuthecat.coffeebot.settings.Settings;
 
 public class UnSetGuildCommand extends Command {
@@ -23,6 +26,28 @@ public class UnSetGuildCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        event.reply(this.name + " is WIP");
+        String settingName = event.getArgs().split(" ")[0];
+        String reply;
+
+        if (settingName.isBlank()) {
+            reply = " You need to specify a setting *and* a value for that setting, DOLT!";
+        } else {
+            SettingEnum settingStatus = settings.removeGuildSetting(event.getGuild(), settingName);
+            switch (settingStatus) {
+                case SUCCCESSFUL:
+                    reply = " The setting has been reset!"; break;
+                case DOESNOTEXIST:
+                    reply = " There is no setting by the name of **" + settingName + "**, check your spelling or something idk."; break;
+                case FORBIDDEN:
+                    reply = " That setting can not be changed at a server level."; break;
+                case NOTSET:
+                    reply = " That setting isn't set for this server, so I guess you can consider it unset?"; break;
+                default:
+                    // Includes SettingEnum.ERROR
+                    reply = " Something went wrong. Not sure what. Not sure I care."; break;
+            }
+        }
+
+        event.getChannel().sendMessage(event.getAuthor().getAsMention() + reply).queue();
     }
 }

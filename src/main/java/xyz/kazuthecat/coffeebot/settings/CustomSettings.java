c26203db333ac5d1dc3c60844eda66371dd733cb
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-30 19:10:33 +0200

    Javadoc added

--- src/main/java/xyz/kazuthecat/coffeebot/settings/CustomSettings.java
+++ src/main/java/xyz/kazuthecat/coffeebot/settings/CustomSettings.java
@@ -6,6 +6,11 @@ import net.dv8tion.jda.api.entities.User;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Class for holding a custom setting. Keeps a record of all individual and server settings as well as if
+ * the default behaviour has been altered (by the bot owner). Also checks if the setting is allowed to be
+ * changed by users/admins before doing so.
+ */
 public class CustomSettings {
     private final String name; // Used only  for error messages.
     private final Map<Long, String> guildValues;
@@ -14,6 +19,13 @@ public class CustomSettings {
     private boolean adminChangeable;
     private String alteredDefault;
 
+    /**
+     * Default constructor.
+     * @param name The name of the setting, mostly used for error messages.
+     * @param userChangeable Boolean value indicating whether users are allowed to change the setting.
+     * @param adminChangeable Boolean value indicating whether server admins are allowed to change the setting.
+     * @param alteredDefault String value with the altered default value, or null.
+     */
     CustomSettings(String name, boolean userChangeable, boolean adminChangeable, String alteredDefault) {
         this.name = name;
         this.userChangeable = userChangeable;
@@ -30,6 +42,13 @@ public class CustomSettings {
     public boolean isAdminChangeable() { return adminChangeable; }
     public String getName() { return name; }
 
+    /**
+     * Retrieves the most relevant setting for a given user/guild combination, if the setting is set for user/admin
+     * this setting is returned. If it is not set the altered default (which may be null) is returned.
+     * @param user The User object which requested the setting.
+     * @param guild the Guild object which requested the setting.
+     * @return The string value for the setting (may be null).
+     */
     String getSetting(User user, Guild guild) {
         String userSetting = userValues.get(user.getIdLong());
         if (userSetting != null && userChangeable) {
@@ -44,24 +63,38 @@ public class CustomSettings {
         return alteredDefault; // May be null!
     }
 
+    /**
+     * Method for setting a new default value.
+     * @param value The value for the new default.
+     */
     void setDefault(String value) {
         alteredDefault = value;
     }
 
+    /**
+     * Method for unsetting an altered default value.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     SettingEnum unsetDefault() {
         if (alteredDefault != null) {
             alteredDefault = null;
-            return SettingEnum.SUCCCESSFUL;
+            return SettingEnum.SUCCESSFUL;
         } else {
             return SettingEnum.NOTSET;
         }
     }
 
+    /**
+     * Method for personalizing the setting for a user.
+     * @param user The user who wishes to customise the setting.
+     * @param value The value which the setting will have for this user.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     SettingEnum putUserSetting(User user, String value) {
         if (userChangeable) {
             try {
                 userValues.put(user.getIdLong(), value);
-                return SettingEnum.SUCCCESSFUL;
+                return SettingEnum.SUCCESSFUL;
             } catch (Exception e) {
                 System.out.println("Error: Could not change setting " + name + " for user " + user.getName());
                 System.out.println(e.toString());
@@ -72,11 +105,16 @@ public class CustomSettings {
         }
     }
 
+    /**
+     * Method for removing the setting made for a given user.
+     * @param user The user who wishes to reset the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     SettingEnum removeUserSetting(User user) {
         try {
             if (userValues.containsKey(user.getIdLong())) {
                 userValues.remove(user.getIdLong());
-                return SettingEnum.SUCCCESSFUL;
+                return SettingEnum.SUCCESSFUL;
             } else {
                 return SettingEnum.NOTSET;
             }
@@ -87,11 +125,17 @@ public class CustomSettings {
         }
     }
 
+    /**
+     * Method for customizing the setting for a given guild.
+     * @param guild The guild thas wishes to customise the setting.
+     * @param value The value which the setting will have for this server/guild.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     SettingEnum putGuildSetting(Guild guild, String value) {
         if (adminChangeable) {
             try {
                 guildValues.put(guild.getIdLong(), value);
-                return SettingEnum.SUCCCESSFUL;
+                return SettingEnum.SUCCESSFUL;
             } catch (Exception e) {
                 System.out.println("Error: Could not change setting " + name + " for guild " + guild.getName());
                 System.out.println(e.toString());
@@ -102,11 +146,16 @@ public class CustomSettings {
         }
     }
 
+    /**
+     * Method for removing the setting made for a given guild.
+     * @param guild The guild that wishes to reset their setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     SettingEnum removeGuildSetting(Guild guild) {
         try {
             if (guildValues.containsKey((guild.getIdLong()))) {
                 guildValues.remove(guild.getIdLong());
-                return SettingEnum.SUCCCESSFUL;
+                return SettingEnum.SUCCESSFUL;
             } else {
                 return SettingEnum.NOTSET;
             }
@@ -117,3 +166,5 @@ public class CustomSettings {
         }
     }
 }
+
+
--- src/main/java/xyz/kazuthecat/coffeebot/settings/Settings.java
+++ src/main/java/xyz/kazuthecat/coffeebot/settings/Settings.java
@@ -1,7 +1,6 @@
 package xyz.kazuthecat.coffeebot.settings;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
@@ -18,12 +17,24 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * This class keeps track of all the individual settings, any changes to
+ * the settings goes through this class which will also check if they exist
+ * and carry out a few standard replacements for placeholders such as
+ * {placeholder_name} and the like.
+ */
 public class Settings {
     private final Gson gson;
     private final String settingsFile;
     private final Map<String, String> defaultSettings;
     private Map<String, CustomSettings> customSettings;
 
+    /**
+     * The constructor for the settings class. When called it will look for
+     * any existing settings and load them. If the settings can't load for any
+     * reason (such as if they don't exist) it will output a message saying
+     * something went wrong and then create a new set of settings.
+     */
     public Settings() {
         gson = new Gson();
         settingsFile = "botsettings.json";
@@ -38,9 +49,22 @@ public class Settings {
         } catch (Exception e) {
             System.out.println("Something went wrong when trying to load json.");
         }
-        if (customSettings == null) customSettings = new HashMap<>();
+
+        if (customSettings == null) {
+            customSettings = new HashMap<>();
+        }
     }
 
+    /**
+     * This function is called by each command that wants to add it's settings. Each call creates
+     * one setting if it was not among the settings loaded at start. If the setting already exists
+     * it's attributes adminChangeable and userChangeable are overwritten with those provided by
+     * the command adding it's settings.
+     * @param identifier The name of the setting, e.g. something like hello.response.
+     * @param defaultValue The default value for the setting (unless changed though putBotSetting).
+     * @param userChangeable Boolean saying whether users are allowed to change this setting.
+     * @param adminChangeable Boolean saying whether server admins are allowed to change this setting.
+     */
     public void setDefaults(String identifier, String defaultValue, boolean userChangeable, boolean adminChangeable) {
         defaultSettings.put(identifier, defaultValue);
 
@@ -53,6 +77,9 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for writing the contents of customSettings to disk in JSON format.
+     */
     private void writeJSON() {
         // Pretty print json, for debugging
         // System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(customSettings));
@@ -68,6 +95,13 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for setting a new default value for a setting. This value will be used in all servers as long
+     * as users don't have their own settings or admins have set a server setting.
+     * @param identifier The name of the setting.
+     * @param value The new value for the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum putBotSetting(String identifier, String value) {
         if (defaultSettings.containsKey(identifier)) {
             try {
@@ -75,7 +109,7 @@ public class Settings {
                         .get(identifier)
                         .setDefault(value);
                 writeJSON();
-                return SettingEnum.SUCCCESSFUL;
+                return SettingEnum.SUCCESSFUL;
             } catch (Exception e) {
                 System.out.println("Failed to change bot setting +" + identifier + ":");
                 System.out.println(e.toString());
@@ -85,6 +119,11 @@ public class Settings {
         return SettingEnum.DOESNOTEXIST;
     }
 
+    /**
+     * Method for resetting a changed default value to the default provided by the command itself.
+     * @param identifier The name of the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum removeBotSetting(String identifier) {
         if (defaultSettings.containsKey(identifier)) {
             return customSettings
@@ -94,6 +133,13 @@ public class Settings {
         return SettingEnum.DOESNOTEXIST;
     }
 
+    /**
+     * Method for changing a setting for an individual user.
+     * @param user The User object for the user.
+     * @param identifier The name of the setting.
+     * @param value The new value for the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum putUserSetting(User user, String identifier, String value) {
         if (defaultSettings.containsKey(identifier)) {
             SettingEnum result = customSettings
@@ -106,6 +152,12 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for resetting a setting for an individual user.
+     * @param user The User object for the user.
+     * @param identifier The name of the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum removeUserSetting(User user, String identifier) {
         if (defaultSettings.containsKey(identifier)) {
             SettingEnum result = customSettings
@@ -118,6 +170,13 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for changing a setting for a specific server.
+     * @param guild The Guild object for the server.
+     * @param identifier The name of the setting.
+     * @param value The new value for the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum putGuildSetting(Guild guild, String identifier, String value) {
         if (defaultSettings.containsKey(identifier)) {
             SettingEnum result = customSettings
@@ -130,6 +189,12 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for resetting a setting for a specific server.
+     * @param guild The Guild object for the server.
+     * @param identifier The name of the setting.
+     * @return A SettingEnum indicating whether the operation was successful.
+     */
     public SettingEnum removeGuildSetting(Guild guild, String identifier) {
         if (defaultSettings.containsKey(identifier)) {
             SettingEnum result = customSettings
@@ -142,6 +207,16 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for retrieving the most relevant setting for responding to a message.
+     * If a user setting is set (and the setting is currently userChangeable), that setting is returned.
+     * If a guild setting is set (and the setting is currently adminChangeable), that setting is returned.
+     * If the default setting has been altered, that setting is returned.
+     * If the setting has not been altered, the default setting is returned.
+     * @param identifier The name of the setting.
+     * @param message The message requesting the setting.
+     * @return A string containing the setting.
+     */
     public String getSetting(String identifier, Message message) {
         if (!defaultSettings.containsKey(identifier)) {
             return null;
@@ -167,6 +242,13 @@ public class Settings {
         }
     }
 
+    /**
+     * Method for carrying out a few standard replacements on a retrieved setting.
+     * Placeholders are indicated with {brackets}.
+     * @param input The string we're doing replacements on.
+     * @param message The message requesting the setting.
+     * @return A modified version of input with all replacements carried out.
+     */
     private String standardReplacements(String input, Message message) {
         return input
                 .replaceAll("\\{user\\}", message.getAuthor().getAsMention())
@@ -174,6 +256,11 @@ public class Settings {
                 .replaceAll("\\{content\\}", message.getContentRaw().strip());
     }
 
+    /**
+     * Method for searching through the settings by looking for setting names containing the provided substring.
+     * @param substring The substring we're filtering settings based on.
+     * @return A set of CustomSettings whose names contain the provided substring.
+     */
     public Set<CustomSettings> allSettingsContaining(String substring) {
         return customSettings.entrySet().stream()
                 .filter(x -> x.getKey().contains(substring))

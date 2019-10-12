ceccdf7c991f392e75fe35d93403d42419e3aec4
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-10-12 04:34:23 +0200

    added DBHandler method query for querying the database

--- src/main/java/xyz/kazuthecat/coffeebot/databases/DBHandler.java
+++ src/main/java/xyz/kazuthecat/coffeebot/databases/DBHandler.java
@@ -1,7 +1,10 @@
 package xyz.kazuthecat.coffeebot.databases;
 
-import javax.swing.plaf.nimbus.State;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DBHandler {
     private final String dbAddr;
@@ -10,11 +13,19 @@ public class DBHandler {
         this.dbAddr = "jdbc:mysql://" + dbAddr + "/coffeedb?user=" + dbUser + "&password=" + dbPass;
     }
 
+    /**
+     * Get a Connection object for the database.
+     * @return Connection object.
+     * @throws SQLException If a connection can not be established.
+     * @throws ClassNotFoundException If the MySQL JDBC Driver can not be found.
+     */
     private Connection getDBConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(dbAddr);
     }
 
+    // TODO Try maybe combining the sqls into a single statement.
+    // TODO Not sure if possible, but worth a shot.
     /**
      * Execute an arbitrary number of SQL statements.
      * @param sqls The statements to be executed.
@@ -72,6 +83,53 @@ public class DBHandler {
         return dbEnum;
     }
 
+    /**
+     * Execute a query.
+     * @param sql The query to be executed.
+     * @return The ResultSet from the query.
+     */
+    public List<Map<String, String>> query(String sql) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+        List<Map<String, String>> result = new ArrayList<>();
+
+        try {
+            conn = getDBConnection();
+            stmt = conn.createStatement();
+            rset = stmt.executeQuery(sql);
+
+            // Parsing the results into a list of HashMap<String, String>
+            ResultSetMetaData rsetMeta = rset.getMetaData();
+            String[] columns = new String[rsetMeta.getColumnCount()];
+
+            for (int i = 0; i < columns.length; i++) {
+                columns[i] = rsetMeta.getColumnLabel(i + 1);
+            }
+
+            while (rset.next()) {
+                Map<String, String> nextResult = new HashMap<>();
+                for (String column : columns) {
+                    nextResult.put(column, rset.getString(column));
+                }
+                result.add(nextResult);
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            executeException(e);
+        } finally {
+            try { if (stmt != null) stmt.close(); } catch (Exception e) { executeException(e); }
+            try { if (conn != null) conn.close(); } catch (Exception e) { executeException(e); }
+            try { if (rset != null) rset.close(); } catch (Exception e) { executeException(e); }
+        }
+        return result;
+    }
+
+    /**
+     * Exception handling to avoid duplicate code in other functions.
+     * @param e The Exception we are handling.
+     * @return Always DBEnum.ERROR.
+     */
     private DBEnum executeException(Exception e) {
         // Idea is that we might expand this to give more specific Enums later on.
         if (e instanceof SQLException) {
